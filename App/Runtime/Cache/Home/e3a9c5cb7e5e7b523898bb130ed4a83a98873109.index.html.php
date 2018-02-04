844feab151ef0b5e108b1bd17b6a2eff62a434c1
beyondamos <328122186@qq.com>
2018-02-04 08:47:25 +0800

    测试

---
+++ App/Runtime/Cache/Home/e3a9c5cb7e5e7b523898bb130ed4a83a98873109.index.html.php
@@ -0,0 +1,32 @@
+<?php /*%%SmartyHeaderCode:126595a7657fce4bed6-79573046%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'e3a9c5cb7e5e7b523898bb130ed4a83a98873109' => 
+    array (
+      0 => 'D:\\phpStudy\\WWW\\ming\\App\\Home\\View\\Index\\index.html',
+      1 => 1517472705,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '126595a7657fce4bed6-79573046',
+  'variables' => 
+  array (
+    'aaa' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.6',
+  'unifunc' => 'content_5a7657fce9a0f',
+  'cache_lifetime' => 3600,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5a7657fce9a0f')) {function content_5a7657fce9a0f($_smarty_tpl) {?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	这是一个测试模版！
+	小明
+</body>
+</html><?php }} ?>
---
+++ App/Runtime/Compile/Home/e3a9c5cb7e5e7b523898bb130ed4a83a98873109.file.index.html.cache.php
@@ -0,0 +1,37 @@
+<?php /* Smarty version Smarty-3.1.6, created on 2018-02-04 08:46:52
+         compiled from "D:\phpStudy\WWW\ming\App\Home\View\Index\index.html" */ ?>
+<?php /*%%SmartyHeaderCode:126595a7657fce4bed6-79573046%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'e3a9c5cb7e5e7b523898bb130ed4a83a98873109' => 
+    array (
+      0 => 'D:\\phpStudy\\WWW\\ming\\App\\Home\\View\\Index\\index.html',
+      1 => 1517472705,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '126595a7657fce4bed6-79573046',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'aaa' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.6',
+  'unifunc' => 'content_5a7657fce8686',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5a7657fce8686')) {function content_5a7657fce8686($_smarty_tpl) {?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	这是一个测试模版！
+	<?php echo $_smarty_tpl->tpl_vars['aaa']->value;?>
+
+</body>
+</html><?php }} ?>
